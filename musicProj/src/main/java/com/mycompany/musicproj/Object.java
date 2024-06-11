355c78aa46ff0ac32ebaa5983856048a5b267c22
peter <peter@10.242.165.183>
2024-06-11 13:01:13 -0400

    Merge origin/master
    # Conflicts:
    #	src/linkedliststarter/ILinkedList.java

---
+++ musicProj/src/main/java/com/mycompany/musicproj/Object.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.musicproj;
+
+/**
+ *
+ * @author peter
+ */
+public class Object {
+    boolean isInstrument;
+    String objectName;
+}
