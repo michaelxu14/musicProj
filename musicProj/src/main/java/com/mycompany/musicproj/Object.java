85736b661140283d79985d1590fac1ced7151bd6
peter <peter@10.242.165.183>
2024-06-11 13:51:16 -0400

    Merge origin/master
    # Conflicts:
    #	src/linkedliststarter/ILinkedList.java

--- musicProj/src/main/java/com/mycompany/musicproj/Object.java
+++ musicProj/src/main/java/com/mycompany/musicproj/Object.java
@@ -9,6 +9,43 @@ package com.mycompany.musicproj;
  * @author peter
  */
 public class Object {
-    boolean isInstrument;
-    String objectName;
+    private boolean isInstrument;
+    private boolean signedOut;
+    private String objectName;
+    private int identifier;
+    
+    public Object(boolean isInstrument, boolean signedOut, String objectName, int identifier){
+        this.isInstrument = isInstrument;
+        this.signedOut = signedOut;
+        this.objectName = objectName;
+        this.identifier = identifier;
+    }
+
+    /**
+     * @return if it is an instrument or not
+     */
+    public boolean isIsInstrument() {
+        return isInstrument;
+    }
+
+    /**
+     * @return if it is signed out
+     */
+    public boolean isSignedOut() {
+        return signedOut;
+    }
+
+    /**
+     * @return the objectName
+     */
+    public String getObjectName() {
+        return objectName;
+    }
+
+    /**
+     * @return the identifier
+     */
+    public int getIdentifier() {
+        return identifier;
+    }
 }
