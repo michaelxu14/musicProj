53797754e900ee01d43eaa3c9fb335ba1e39ed6c
Michael Xu <michael.xu.14@gmail.com>
2024-06-14 13:36:23 -0400

    Merge origin/main into main

---
+++ musicProj/src/main/java/com/mycompany/musicproj/Credentials.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.musicproj;
+import java.util.Date;
+/**
+ *
+ * @author Xu Last Name
+ */
+    
+public class Credentials {
+    private int circid;
+    private String sbarcode;
+    private String ebarcode;
+    private Date signout;
+    private Date signin;
+
+    // Constructor
+    public Credentials(int circid, String sbarcode, String ebarcode, Date signout, Date signin) {
+        this.circid = circid;
+        this.sbarcode = sbarcode;
+        this.ebarcode = ebarcode;
+        this.signout = signout;
+        this.signin = signin;
+    }
+
+    // Getter and Setter methods
+    public int getCircid() {
+        return circid;
+    }
+
+    public void setCircid(int circid) {
+        this.circid = circid;
+    }
+
+    public String getSbarcode() {
+        return sbarcode;
+    }
+
+    public void setSbarcode(String sbarcode) {
+        this.sbarcode = sbarcode;
+    }
+
+    public String getEbarcode() {
+        return ebarcode;
+    }
+
+    public void setEbarcode(String ebarcode) {
+        this.ebarcode = ebarcode;
+    }
+
+    public Date getSignout() {
+        return signout;
+    }
+
+    public void setSignout(Date signout) {
+        this.signout = signout;
+    }
+
+    public Date getSignin() {
+        return signin;
+    }
+
+    public void setSignin(Date signin) {
+        this.signin = signin;
+    }
+}
